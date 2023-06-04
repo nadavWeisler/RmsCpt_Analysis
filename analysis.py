8165f5c8e9968c191750c9a30419542802910ec1
Nadav Weisler <32937058+nadavWeisler@users.noreply.github.com>
2023-06-04 21:28:59 +0300

    do analysis

--- analysis.py
+++ analysis.py
@@ -50,6 +50,18 @@ def plot_two_columns_with_regression_line_and_color(df, x, y, color):
     plt.plot(X, Y_pred, color='red')
     plt.show()
 
+def get_normelaized_df(df_origin, x, y, sd=2.5):
+        df = df_origin.copy()
+        x_mean = df[x].mean()
+        x_sd = df[x].std()
+        y_mean = df[y].mean()
+        y_sd = df[y].std()
+        df = df[(df[x] > (x_mean - (sd * x_sd))) &
+                (df[x] < (x_mean + (sd * x_sd)))]
+        df = df[(df[y] > (y_mean - (sd * y_sd))) &
+                (df[y] < (y_mean + (sd * y_sd)))]
+        return df
+
 
 def add_regression_line(df, ax, x, y, color='red'):
     X = df[x].values.reshape(-1, 1)
@@ -83,18 +95,7 @@ def plot_multiple_two_columns_with_regression_line(plots, in_line=3):
             " N: " + str(len(current_df))
         ax.set_title(title)
 
-    def get_normelaized_df(df_origin, x, y, sd=2.5):
-        df = df_origin.copy()
-        x_mean = df[x].mean()
-        x_sd = df[x].std()
-        y_mean = df[y].mean()
-        y_sd = df[y].std()
-        df = df[(df[x] > (x_mean - (sd * x_sd))) &
-                (df[x] < (x_mean + (sd * x_sd)))]
-        df = df[(df[y] > (y_mean - (sd * y_sd))) &
-                (df[y] < (y_mean + (sd * y_sd)))]
-        return df
-
+    
     cols = in_line
     rows = math.ceil(len(plots) / in_line)
     fig, axis = plt.subplots(rows, cols, figsize=(20, 8 * rows))
@@ -128,4 +129,25 @@ def plot_multiple_two_columns_with_regression_line(plots, in_line=3):
             add_title(axis[x_axis, y_axis], x, y, fix_outliers, dataset_name)
             add_regression_line(current_df, axis[x_axis, y_axis], x, y)
 
+    plt.show()
+    
+def plot_two_cols_with_regression_line(df, x, y, x_label, y_label):
+    fig, ax = plt.subplots(figsize=(10, 10))
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    current_df = pd.DataFrame()
+    current_df = get_normelaized_df(df.copy()[[x, y]], x, y)
+    current_df.dropna(inplace=True)
+    ax.scatter(
+        current_df[x], current_df[y], cmap='viridis')
+    try:
+        p_value = pearsonr(current_df[x], current_df[y])[1]
+        round_index = get_round_index(p_value)
+        p_value_str = str(
+            round(pearsonr(current_df[x], current_df[y])[1], round_index))
+    except:
+        p_value_str = "null"
+    ax.set_title('P value: ' + p_value_str + " Corr: " + str(
+        round(current_df[x].corr(current_df[y]), 4)) + " N: " + str(len(current_df)), fontsize=20)
+    add_regression_line(current_df, ax, x, y)
     plt.show()
